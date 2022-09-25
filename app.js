b735d20ad98606f6897dbf624a8a2b4ad2e55292
DG02 <72976359+Dibyajyoti2002@users.noreply.github.com>
2022-09-25 09:03:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,6 @@ app.get("/jsob/:name",function(req,res){
 })
 
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
